bfb9c6019ee4d82a4ca1297e04d31f1377269bac
Leon Do <leondo123@gmail.com>
2023-01-05 17:17:45 -0500

    Create NFKey.sol

---
+++ NFKey.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract NFKey {
+    /*
+    Product Flow
+    - Creator generates a random private key called NFKey off-chain
+    - Creator gets Item Id from NFTKey by calling nfKeyToItemId() off-chain
+    - Creator calls create(itemId) to prove this item is authentic aka "I created this. Here's proof on chain"
+    - Creator attaches NFKey to NFC and attaches to swag
+    - Buyer buys swag that has NFKey
+    - Buyer gets Auth Key from NFTKey by calling nfKeyToAuthKey() off-chain
+    - Auth Key is used to authenticate the purchase aka "I bought this"
+    - Buyer authenticates by submitting Auth Key on-chain aka "Item #4 was auth on Jan 1"
+
+    How to prove authenticity
+    - If the buyer duplicates the NFC and sells fake swag
+    - The scammed buyer will try and authenticate
+    - On authenticate, they will recieve a message "Item already authenticated"
+    */
+
+    // stores info about item
+    struct ItemId { 
+        address creator;
+        bool isAuthenticated;
+    }
+
+    // maps authKey => ItemId
+    mapping (bytes32 => ItemId) public itemIds;
+
+    // events
+    event Create(bytes32 _itemId, address _creator);
+    event Authenticate(bytes32 _itemId, bool _isAuthenticated);
+
+    // anyone can create an item
+    function create(bytes32 _itemId) public {
+        require(itemIds[_itemId].creator == address(0x0), "Item already created");
+        itemIds[_itemId].creator = msg.sender;
+        emit Create(_itemId, msg.sender);
+    }
+
+    // purchaser can authenticate
+    function authenticate(bytes32 _authKey) public {
+        bytes32 itemId = authKeyToItemId(_authKey);
+        require(itemIds[itemId].isAuthenticated == false, "Item already authenticated");
+        itemIds[itemId].isAuthenticated = true;
+        emit Authenticate(itemId, true);
+    }
+
+    /*
+    * Hashing functions
+    * nfKey -> authKey -> itemId
+    * nfKey     -- nfKeyToAuthKey() --> authKey
+    * authKey   -- authKeyToItemId()--> itemId
+    * nfKey     -- nfKeyToItemId()  --> itemId
+    * @example hello -> 0x2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824 -> 0x9595c9df90075148eb06860365df33584b75bff782a510c6cd4883a419833d50
+    */
+    // hashes nfKey to authKey
+    function nfKeyToAuthKey(string memory _nfKey) public pure returns (bytes32) {
+        bytes32 authKey = sha256(abi.encodePacked(_nfKey));
+        return authKey;
+    }
+    // hashes authKey to ItemId
+    function authKeyToItemId(bytes32 _authKey) public pure returns(bytes32){
+        bytes32 itemId = sha256(abi.encodePacked(_authKey));
+        return itemId;
+    }
+    // hashes nfKey to itemId
+    function nfKeyToItemId(string memory _nfKey) public pure returns(bytes32){
+        bytes32 authKey = sha256(abi.encodePacked(_nfKey));
+        bytes32 itemId = sha256(abi.encodePacked(authKey));
+        return itemId;
+    }
+
+}
